7604713ed5c9d1ed29db0e33b585da651286ee7b
Falxhor <joren_broekema@hotmail.com>
2017-02-07 13:12:32 +0100

    added trigger switch cases and logging

--- app.js
+++ app.js
@@ -5,21 +5,11 @@ const language = Homey.manager('i18n').getLanguage();
 module.exports.init = function init() {
 
 	Homey.log("Hello world!");
-	Homey.manager('ledring').animate(
-		// animation name (choose from loading, pulse, progress, solid)
-		'loading',
-
-		// optional animation-specific options
+	Homey.manager('ledring').animate('loading',
 		{
 			color: 'purple',
 			rpm: 10 // change rotations per minute
-		},
-
-		// priority
-		'INFORMATIVE',
-
-		// duration
-		30000,
+		},'INFORMATIVE', 30000,
 
 		// callback
 		function( err, success ) {
@@ -28,8 +18,11 @@ module.exports.init = function init() {
 		}
 	);
 	Homey.manager('speech-output').say("Hello world");
+}
+
+function listenForSpeechEvents(){
 	Homey.manager('speech-input').on('speech', speech => {
-		console.log("Incoming speech event..");
+		Homey.log("Incoming speech event..");
 
 		const options = {
 				calculateTrigger: false,
@@ -39,13 +32,13 @@ module.exports.init = function init() {
 				dividedTrigger: false,
 				language : language,
 				dateTranscript: (language === 'en') ? 'calculate' : 'bereken',
-		},
+		};
 
 		const timeout = setTimeout(() => {
 			options.abort = true;
 		}, 10000);
 
-		if(!abort) parseSpeechTriggers(speech, options);
+		if(!options.abort) parseSpeechTriggers(speech, options);
 
 		//speech.say("Ask me");
 	});
@@ -53,24 +46,38 @@ module.exports.init = function init() {
 }
 
 function parseSpeechTriggers(speech, options){
-
-	speech.triggers.forEach(trigger = > {
+	Homey.log("Checking for triggers..");
+	speech.triggers.forEach(trigger => {
 		switch (trigger.id){
 			case 'calculate':
 				options.calculateTrigger = true;
+				options.dateTranscript = (options.language === 'en') ? 'calculate' : 'bereken';
 				break;
 			case 'plus':
 				options.plusTrigger = true;
+				const textPlus = speech.transcript.substring(trigger.position - 3, trigger.position + 7);
+				var result = textPlus.match( new RegExp( "(\\d+)\\s" + trigger.id + "\\s(\\d+)" ) );
 				break;
 			case 'minus':
 				options.minusTrigger = true;
+				options.dateTranscript = (options.language === 'en') ? 'minus' : 'min';
 				break;
 			case 'times':
 				options.timesTrigger = true;
+				options.dateTranscript = (options.language === 'en') ? 'times' : 'keer';
 			  break;
 			case 'divided':
 				options.dividedTrigger = true;
+				options.dateTranscript = (options.language === 'en') ? 'divided by' : 'gedeeld door';
 				break;
 		}
 	});
 }
+
+/*
+var transcript = "16 plus 32"
+var trigger = {
+    id: "keer"
+}
+var result = transcript.match( new RegExp( "(\\d+)\\s" + trigger.id + "\\s(\\d+)" ) );
+*/
