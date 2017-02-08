ac7666c9d9731bec20b1e14d0e5128ddf6b74746
Falxhor <joren_broekema@hotmail.com>
2017-02-08 16:11:45 +0100

    removed some unnecessary code

--- app.js
+++ app.js
@@ -3,27 +3,45 @@
 const language = Homey.manager('i18n').getLanguage();
 
 module.exports.init = function init() {
-	Homey.log("Hello world!");
-	console.log(language);
-	Homey.manager('ledring').animate('loading',
-		{
-			color: 'purple',
-			rpm: 10 // change rotations per minute
-		},'INFORMATIVE', 3000,
 
-		// callback
-		function( err, success ) {
-			if( err ) return Homey.error(err);
-			console.log("Animation played succesfully");
-		}
-	);
 	Homey.manager('speech-input').on('speech', listenForSpeechEvents);
+
+	Homey.manager('flow').on('action.addition', function(callback, args){
+		var numberOne = args.numberone;
+		var numberTwo = args.numbertwo;
+		var result = numberOne + numberTwo;
+		callback( null, true );
+		Homey.manager('speech-output').say(result.toString());
+	});
+
+	Homey.manager('flow').on('action.subtraction', function(callback, args){
+		var numberOne = args.numberone;
+		var numberTwo = args.numbertwo;
+		var result = numberOne - numberTwo;
+		callback( null, true );
+		Homey.manager('speech-output').say(result.toString());
+	});
+
+	Homey.manager('flow').on('action.multiplication', function(callback, args){
+		var numberOne = args.numberone;
+		var numberTwo = args.numbertwo;
+		var result = numberOne * numberTwo;
+		callback( null, true );
+		Homey.manager('speech-output').say(result.toString());
+	});
+
+	Homey.manager('flow').on('action.division', function(callback, args){
+		var numberOne = args.numberone;
+		var numberTwo = args.numbertwo;
+		var result = numberOne / numberTwo;
+		callback( null, true );
+		Homey.manager('speech-output').say(result.toString());
+	});
 }
 
 function listenForSpeechEvents(speech){
 		console.log("Incoming speech event..");
 		const options = {
-				calculateTrigger: false,
 				language : language,
 				dateTranscript: (language === 'en') ? 'calculate' : 'bereken',
 		};
@@ -31,7 +49,6 @@ function listenForSpeechEvents(speech){
 		const timeout = setTimeout(() => {
 			options.abort = true;
 		}, 10000);
-
 		if(!options.abort) parseSpeechTriggers(speech, options);
 }
 
@@ -39,38 +56,32 @@ function parseSpeechTriggers(speech, options){
 	const speechText = speech.transcript;
 	var speechReg = speechText.match( new RegExp( "(\\d+)\\s\\w+\\s?\\w*\\s(\\d+)" ) );
 	var result = null;
-	if(speechReg){
-		speech.triggers.forEach(trigger => {
+
+	if(speechReg){ // did we find a match?
+        speech.triggers.forEach(function (trigger){
 			switch (trigger.id){
-				case 'calculate':
-					options.calculateTrigger = true;
-					options.dateTranscript = (options.language === 'en') ? 'calculate' : 'bereken';
-					break;
 				case 'plus':
 					result = parseInt(speechReg[1]) + parseInt(speechReg[2]);
 					break;
 				case 'minus':
-					options.dateTranscript = (options.language === 'en') ? 'minus' : 'min';
 					result = parseInt(speechReg[1]) - parseInt(speechReg[2]);
 					break;
 				case 'times':
-					options.dateTranscript = (options.language === 'en') ? 'times' : 'keer';
 					result = parseInt(speechReg[1]) * parseInt(speechReg[2]);
 				  break;
 				case 'divided':
-					options.dateTranscript = (options.language === 'en') ? 'divided by' : 'gedeeld door';
 					result = parseInt(speechReg[1]) / parseInt(speechReg[2]);
 					break;
 			}
 		});
 	}else{
-		parseSpeechResponse(speech, null);
+		parseSpeechResponse(speech, null, options);
 	}
-
 	parseSpeechResponse(speech, result, options);
 }
 
 function parseSpeechResponse(speech, result, options){
+	console.log("Creating response..");
 	var repeatText = (language === 'en') ? 'Can you repeat that please' : 'Kun je dat herhalen';
 	if(result == null) {
 		speech.say(repeatText);
